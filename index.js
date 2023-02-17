fe965336813fda1a9b5673576786f0a58a21ce16
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-02-16 18:36:09 -0600

    Fix radio button and add delet function

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-var myLibrary = []
+let myLibrary = []
 
-const addBookToLibrary = (ev)=>{
-    ev.preventDefault();
-    let book = {
-        title: document.getElementById('title').value,
-        author: document.getElementById('author').value,
-        status: document.getElementById('status').value
-    }
-    myLibrary.push(book)
+function book(title, author, read){
+    this.title = title
+    this.author = author
+    this.read = read
+}
+
+
+
+function addBookToLibrary(){
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let read = document.querySelector('#read').checked;
+    let newBook = new book(title, author, read)
+    myLibrary.push(newBook)
+    buildTable();
     document.forms[0].reset();
 }
 
-buildTable(myLibrary)
-
-
-function buildTable(data){
-    var table = document.getElementById('myTable')
-
-    for (var i = 0; i < data.length; i++){
-        var row = `<tr>
-                        <td>${data[i].title}</td>
-                        <td>${data[i].author}</td>
-                        <td>${data[i].status}</td>
+function buildTable(){
+    let table = document.getElementById('myTable')
+    table.innerHTML =''
+    for (let i = 0; i < myLibrary.length; i++){
+        let row = `<tr>
+                        <td>${myLibrary[i].title}</td>
+                        <td>${myLibrary[i].author}</td>
+                        <td>${myLibrary[i].read}</td>
+                        <td><button onclick="removeBook(${i})">Remove</button></td> 
                   </tr>`
         table.innerHTML += row
-
-
     }
 }
 
+function removeBook(index){
+    myLibrary.splice(index, 1);
+    buildTable();
+}
 document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('btn').addEventListener('click', addBookToLibrary);
+    document.getElementById('btn').addEventListener('click', function(e){
+        e.preventDefault();
+        addBookToLibrary();
+    });
 });
-
-// {'title':'The Hobbit', 'author':'Tolkein', 'status':'Read'}
